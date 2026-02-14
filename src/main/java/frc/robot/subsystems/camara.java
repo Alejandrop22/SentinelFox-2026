f9222326e38a3e07ca75b6849616cfdcaec62d49
Alejandro <pineda.alejandro@cetys.edu.mx>
2026-02-13 17:55:13 -0800

    Segundo commit del código FRC

--- src/main/java/frc/robot/subsystems/camara.java
+++ src/main/java/frc/robot/subsystems/camara.java
@@ -1,20 +1,67 @@
 package frc.robot.subsystems;
 
-public class camara {
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import org.photonvision.PhotonCamera;
+import org.photonvision.targeting.PhotonPipelineResult;
+import org.photonvision.targeting.PhotonTrackedTarget;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+
+public class Camara extends SubsystemBase {
+
+    public final PhotonCamera camera = new PhotonCamera("photonvision");
 
     private double x_distance;
+    private double y_distance;
+    private double distance_to_target;
+    private double angle_to_target;
 
 
 
-    periodic();
-
-    set 
-
-
-    
-    double getdistance(){
+    public double getDistance() {
         return x_distance;
     }
+
+    public double getYDistance() {
+        return y_distance;
+    }
+
+    public double getAngleToTarget() {
+        return angle_to_target;
+    }
+
+    public double getDistanceToTarget() {
+        return distance_to_target;
+    }
+
+
+    @Override
+    public void periodic() {
+        PhotonPipelineResult result = camera.getLatestResult();
+
+        if (result.hasTargets()) {
+            PhotonTrackedTarget target = result.getBestTarget();
+            x_distance = target.getBestCameraToTarget().getX();
+            y_distance = target.getBestCameraToTarget().getY();
+            distance_to_target = target.getBestCameraToTarget().getZ();
+            angle_to_target = target.getYaw();
+
+            SmartDashboard.putNumber("X Distance", x_distance);
+            SmartDashboard.putNumber("Y Distance", y_distance);
+            SmartDashboard.putNumber("Distance to Target", distance_to_target);
+            SmartDashboard.putNumber("Angle to Target", angle_to_target);
+        } else {
+            x_distance = 0;
+            y_distance = 0;
+            distance_to_target = 0;
+            angle_to_target = 0;
+
+            SmartDashboard.putNumber("X Distance", x_distance);
+            SmartDashboard.putNumber("Y Distance", y_distance);
+            SmartDashboard.putNumber("Distance to Target", distance_to_target);
+            SmartDashboard.putNumber("Angle to Target", angle_to_target);
+        }
+    }
+
 
     
 }
