3467e40c20878ed57bb942a06f80f04c95d9dcfb
unknown <Castillo.francisco@cetys.edu.mx>
2026-02-16 13:02:18 -0800

    Septimo Commit de Paco

--- src/main/java/frc/robot/Main.java
+++ src/main/java/frc/robot/Main.java
@@ -1,7 +1,7 @@
 // Copyright (c) FIRST and other WPILib contributors.
 // Open Source Software; you can modify and/or share it under the terms of easter egg
 // the WPILib BSD license file in the root directory of this project. skibiditoilet
-//penepenepenepenepenepenepenepenepenepene
+
 package frc.robot;
 
 import edu.wpi.first.wpilibj.RobotBase;
