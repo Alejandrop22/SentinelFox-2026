b26a7b97c74f44cc354b3a8e9da7658c57ec1924
Alejandro <pineda.alejandro@cetys.edu.mx>
2026-02-16 20:02:53 -0800

    Octavo Commit de Alejandro

--- src/main/java/frc/robot/subsystems/Intake.java
+++ src/main/java/frc/robot/subsystems/Intake.java
@@ -8,15 +8,15 @@ public class Intake extends SubsystemBase {
 	private final SparkMax m_intakeMotor = new SparkMax(55, MotorType.kBrushless);
 
 	public void intakeReverse() {
-	m_intakeMotor.set(-0.6);
+	m_intakeMotor.set(0.6);
 	}
 
 	public void intakeFullReverse() {
-	m_intakeMotor.set(-1.0);
+	m_intakeMotor.set(1.0);
 	}
 
 	public void intakeForward() {
-	m_intakeMotor.set(0.4);
+	m_intakeMotor.set(-0.4);
 	}
 
 	public void stop() {
